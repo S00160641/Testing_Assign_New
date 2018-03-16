040197487f0a7d5571462c669da94518937951db
Sophia Price <sophiaprice10@hotmail.com>
2018-03-16 14:38:03 +0000

    Front End Added

---
+++ FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using TestingAssignmentFit;
+using TestingAssignment;
+
+namespace FrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InputForm(int age, string gender)
+        {
+            var test = new Program();
+            ViewBag.Message = test.CalcPremium(age, gender);
+            return View();
+        }
+    }
+}
